6e31d995f9738f03bcbc76ad6d8f80a08bb1a671
dodocha169 <dodocha169@icloud.com>
2021-04-08 15:59:18 +0900

    First commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="../banana.png"/>
+        <h1>
+          dodocha169
+        </h1>
+        <a
+          className="App-link"
+          href="https://github.com/dodocha169"
+          target="_blank"
+          rel="noopener noreferrer"
+
+        >
+          GitHub
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
